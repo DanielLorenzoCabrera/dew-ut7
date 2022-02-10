1897707fb92aaf924fd6ee2eecca3c96b7483f5f
Daniel Lorenzo <juandaniellorenzocabrera@alumno.ieselrincon.es>
2022-02-10 19:51:18 +0000

    Primera acabada

--- opener.js
+++ opener.js
@@ -8,24 +8,36 @@ function obtenerBotones(){
     (document.querySelector('.con-caracteristicas')).addEventListener("click", conCaracteristica);
     (document.querySelector('.con-mas-caracteristicas')).addEventListener("click", conCaracteristica);
     (document.querySelector('.pantalla-completa')).addEventListener("click", pantallaCompleta);
-    
-    
-
-
+    (document.querySelector('.cerrar')).addEventListener("click", cerrarTodo);
 
 }
+
+const ventanas = [];
 
 
 
 function noCaracteristica(){
-   const a =  window.open("ventanas/no-caracteristica.html","sin-características","");
-   console.log(a);
+    let veces = this.className === 'no-caracteristicas-una' ? 1 : 5;
+    for(let i = 0; i < veces; i++){
+        const ventana = window.open("ventanas/ventana_emergente.html", `sin-catacteristicas-${i}`, "");
+        ventanas.push(ventana);
+    }
 }
 
 function conCaracteristica(){
-    console.log(this)
+    const ventana = this.className === 'con-caracteristicas' 
+    ? window.open("ventanas/ventana_emergente.html", `con-catacteristicas`, "height=430,width=420,status=yes,resizable=yes,menubar=no")
+    : window.open("ventanas/ventana_emergente.html", `con-mas-catacteristicas`, "height=300,width=600,status=no,resizable=yes,menubar=no,left=150px,scrollbars=no,titlebar=no,location=no,top=");
+    ventanas.push(ventana);
 }
 
 function pantallaCompleta(){
-    console.log(this)
+   const ventana=  window.open("ventanas/ventana_emergente.html", `full-screen`, "outerHeight=200,outerWidth=200,top=0,left=0");
+   ventanas.push(ventana);
+}
+
+function cerrarTodo(){
+    ventanas.forEach(ventana=>{
+        ventana.close();
+    })
 }
---
+++ ventanas/ventana.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", obtenerBotones);
+
+
+function obtenerBotones(){
+    
+    if(window.name === 'full-screen'){
+        document.querySelector('.botones').innerHTML += `<p>Actualmente el fullscreen no está permitido debido a las molestias que genera al usuario</p>`;
+    }
+
+
+
+    const creador =  document.querySelector('.creador');
+    creador.addEventListener("click", ()=>{
+        document.querySelector('h1').innerHTML = "Página creada desde /opener.html";
+    })
+
+    const nombre =  document.querySelector('.nombre');
+    nombre.addEventListener("click", ()=>{
+        document.querySelector('h1').innerHTML = window.name;
+    })
+
+
+    const cerrar =  document.querySelector('.cerrar');
+    cerrar.addEventListener("click", ()=>{
+        window.close();
+        })
+}
